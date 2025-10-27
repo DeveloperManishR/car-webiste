6e8baa317a5e4028c9809e17902d41677de7af18
DeveloperManishR <manishrawat0711@gmail.com>
2025-10-27 11:42:15 +0530

    ignore

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <App />
-      <ToastContainer
+      <ToastContainer 
         position="top-right"
         autoClose={5000}
         hideProgressBar={false}
